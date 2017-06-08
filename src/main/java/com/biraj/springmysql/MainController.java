9d2ae34974f71297c1203a318b62e889e21cece0
Biraj Dhungel <bdhungel@novomind.com>
2017-06-08 17:22:24 +0200

    thymeleaf form get and post

--- src/main/java/com/biraj/springmysql/MainController.java
+++ src/main/java/com/biraj/springmysql/MainController.java
@@ -6,7 +6,10 @@ import java.util.Map;
 import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,17 +21,31 @@ public class MainController {
   @Autowired
   private UserRepository userRepository;
 
-  @GetMapping(path="/add") // Map ONLY GET Requests
-  public @ResponseBody String addNewUser (@RequestParam String name
-      , @RequestParam String email) {
+//  @GetMapping(path="/add") // Map ONLY GET Requests
+//  public @ResponseBody String addNewUser (@RequestParam String name
+//      , @RequestParam String email) {
+//    // @ResponseBody means the returned String is the response, not a view name
+//    // @RequestParam means it is a parameter from the GET or POST request
+//
+//    User n = new User();
+//    n.setName(name);
+//    n.setEmail(email);
+//    userRepository.save(n);
+//    return "Saved";
+//  }
+  
+  @GetMapping(path="/adduser") // Map ONLY GET Requests
+  public String addNewUser (Model model) {
     // @ResponseBody means the returned String is the response, not a view name
     // @RequestParam means it is a parameter from the GET or POST request
-
-    User n = new User();
-    n.setName(name);
-    n.setEmail(email);
-    userRepository.save(n);
-    return "Saved";
+    model.addAttribute("user",new User());
+    return "adduser";
+  }
+  
+  @PostMapping(path="/adduser") // Map ONLY Post Requests
+  public String saveNewUser (@ModelAttribute User user) {
+    userRepository.save(user);    
+    return "adduser";
   }
   
   @GetMapping(path="/all")
